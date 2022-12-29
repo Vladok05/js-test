c686ae34418fc985aba3ad1f198da1a0c9144324
Vladyslav Mishchuk <v.mishuk2017@gmail.com>
2022-12-29 19:43:17 +0000

    test

---
+++ js/test.js
@@ -0,0 +1,12 @@
+const players = [
+    { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+    { name: "Poly", playtime: 469, gamesPlayed: 2 },
+    { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+    { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+  ];
+  // Change code below this line
+  
+const totalAveragePlaytimePerGame = players.reduce((previousValue, player, playtime, gamesPlayed) => previousValue + (player = player.playtime / player.gamesPlayed), 0)
+  console.log( totalAveragePlaytimePerGame)
+                                                  
+  

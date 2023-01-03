b889feecf07e9d0ec1e84508b9acbd03201bff6b
Vladyslav Mishchuk <v.mishuk2017@gmail.com>
2023-01-03 19:34:07 +0000

    test

--- js/test.js
+++ js/test.js
@@ -4,7 +4,7 @@ let orderedQuantity = 6;
 let deliveryFee = 50;
 let totalPrice =  pricePerDroid * orderedQuantity + deliveryFee;
 const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
-console.log(message)p[
+console.log(message)
   
 
 
